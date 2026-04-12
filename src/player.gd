393a6b9de55dbdc353480f7f204d0a2d4581841d
tokss <toxiwe@mail.ru>
2026-04-12 17:45:07 +0300

    Cleanup: delete unnecessarily nodes

--- src/player.gd
+++ src/player.gd
@@ -4,7 +4,7 @@ class_name Player
 @export var input_gatherer : InputGatherer
 @export var model : PlayerModel
 @export var weapon : PlayerWeapon
-@export var head : Node3D
+@export var head : Marker3D
 
 func _physics_process(delta: float) -> void:
 	var input : InputPackage = input_gatherer.gather_input()
--- src/player_weapon.gd
+++ src/player_weapon.gd
@@ -2,7 +2,6 @@ extends Node3D
 class_name PlayerWeapon
 
 @export var weapon_model : WeaponModel
-@export var visuals : WeaponVisuals
 
 func update(input: InputPackage, delta: float) -> void:
 	weapon_model.update(input, delta)
