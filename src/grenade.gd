b501e51fc95c89e642d0750d7566392350f7e3f1
tokss <toxiwe@mail.ru>
2026-04-24 23:42:02 +0300

    Add grenade ability

--- src/grenade.gd
+++ src/grenade.gd
@@ -2,6 +2,27 @@ extends Throwable
 class_name Grenade
 
 @export var particles : GPUParticles3D
+@export var mesh : MeshInstance3D
+@export var debug_mesh : MeshInstance3D
+@export var hurtbox : Hurtbox
+
+var damage : float
+
+func _ready() -> void:
+	super._ready()
+	hurtbox.monitoring = false
+	
+	var attack_data := AttackData.new()
+	damage = 30
+	attack_data.damage = damage
+	hurtbox.attack_data = attack_data
 
 func _on_timer_timeout() -> void:
 	particles.restart()
+	particles.finished.connect(_on_partice_finished, CONNECT_DEFERRED)
+	hurtbox.monitoring = true
+	debug_mesh.visible = true
+	mesh.visible = false
+
+func _on_partice_finished() -> void:
+	queue_free()
--- src/player_abilities/behaviors/throw_ability.gd
+++ src/player_abilities/behaviors/throw_ability.gd
@@ -6,10 +6,10 @@ class_name ThrowAbilityBehavior
 
 func apply_behavior(player: Player) -> void:
 	var direction : Vector3 = (player.head.marker.global_basis * Vector3(0, 0, 1)).normalized()
-	var velocity : Vector3 = direction * throw_power + player.velocity / 5.0 
+	var force : Vector3 = direction * throw_power + player.velocity / 5.0 
 	
 	var item : Throwable = item_scene.instantiate()
-	item.force = velocity
+	item.force = force
 	item.position = player.head.marker.global_position
 	player.get_tree().root.add_child(item)
 	
