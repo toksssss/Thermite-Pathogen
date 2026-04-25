33ba61473d2dcff86d70279cb3f9861f422454b2
tokss <toxiwe@mail.ru>
2026-04-25 12:39:47 +0300

    Add explosion on impact logic to grenade

--- src/grenade.gd
+++ src/grenade.gd
@@ -2,27 +2,34 @@ extends Throwable
 class_name Grenade
 
 @export var particles : GPUParticles3D
-@export var mesh : MeshInstance3D
+@export var mesh_node : Node3D
 @export var debug_mesh : MeshInstance3D
 @export var hurtbox : Hurtbox
 
-var damage : float
+@export var damage : float = 30.0
 
 func _ready() -> void:
-	super._ready()
-	hurtbox.monitoring = false
+	body_entered.connect(_on_body_entered, CONNECT_ONE_SHOT)
+	particles.finished.connect(_on_partice_finished, CONNECT_ONE_SHOT)
 	
+	hurtbox.monitoring = false
+
+func _on_body_entered(_body: Node) -> void:
+	explode()
+
+func explode() -> void:
+	particles.restart()
+	hurtbox.monitoring = true
+	debug_mesh.visible = true
+	mesh_node.visible = false
+
+func create_attack_data() -> void:
 	var attack_data := AttackData.new()
-	damage = 30
 	attack_data.damage = damage
 	hurtbox.attack_data = attack_data
 
-func _on_timer_timeout() -> void:
-	particles.restart()
-	particles.finished.connect(_on_partice_finished, CONNECT_DEFERRED)
-	hurtbox.monitoring = true
-	debug_mesh.visible = true
-	mesh.visible = false
-
 func _on_partice_finished() -> void:
 	queue_free()
+
+func _enter_tree() -> void:
+	apply_impulse(force)
