463790080adca7e67e019ccdd85a871b3b708a6c
tokss <toxiwe@mail.ru>
2026-04-27 16:39:16 +0300

    Add region for navigation tests

--- src/enemies_states/nav_test/idle.gd
+++ src/enemies_states/nav_test/idle.gd
@@ -1,7 +1,5 @@
 extends EnemiesState
 
-var wander_time : float
-
 func enter() -> void:
 	_randomize_wander()
 
@@ -9,10 +7,7 @@ func exit() -> void:
 	pass
 
 func update(delta: float) -> void:
-	if wander_time > 0:
-		wander_time -= delta
-	else:
-		_randomize_wander()
+	pass
 
 func physics_update(_delta: float) -> void:
 	if !npc:
@@ -20,7 +15,7 @@ func physics_update(_delta: float) -> void:
 	
 	if navigation_agent.is_navigation_finished():
 		npc.velocity = Vector3.ZERO
-		return
+		_randomize_wander()
 	
 	var destination := navigation_agent.get_next_path_position()
 	var local_destination := destination - npc.global_position
@@ -28,7 +23,5 @@ func physics_update(_delta: float) -> void:
 	navigation_agent.velocity = direction * 5.0
 
 func _randomize_wander() -> void:
-	var random_direction := Vector3(randf_range(-5, 5), npc.global_position.y, randf_range(-5,5))
-	navigation_agent.set_target_position(random_direction)
-	
-	wander_time = randf_range(1, 3)
+	var random_direction := Vector3(randf_range(-10, 10), npc.position.y, randf_range(-10,10))
+	navigation_agent.set_target_position(npc.global_position - random_direction)
