9eded6868ed275f65b93b6fb29a357ccc8a91cfb
tokss <toxiwe@mail.ru>
2026-05-11 17:19:41 +0300

    Change boost logic in slide move

--- src/player_moves/slide.gd
+++ src/player_moves/slide.gd
@@ -2,15 +2,18 @@ extends Move
 
 var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+const CROUCH_THRESHOLD : float = 7.5
+
+
 func check_relevance(input: InputPackage) -> String:
 	if !player.is_on_floor():
 		return "falling"
 	
-	if player.velocity.length() <= 7.5:
+	if player.velocity.length() <= CROUCH_THRESHOLD:
 			return "crouch"
 	
 	if head_raycast.is_colliding():
-		if player.velocity.length() >= 7.5:
+		if player.velocity.length() >= CROUCH_THRESHOLD:
 			return "okay"
 		else:
 			return "crouch"
@@ -20,8 +23,10 @@ func check_relevance(input: InputPackage) -> String:
 		return "okay"
 	return input.actions[0]
 
+
 func update(input: InputPackage, delta: float) -> void:
 	velocity_by_input(input, delta)
+
 
 func velocity_by_input(input: InputPackage, delta: float) -> void:
 	var direction : Vector3 = (player.global_transform.basis * 
@@ -29,7 +34,7 @@ func velocity_by_input(input: InputPackage, delta: float) -> void:
 	
 	var target : Vector3 = direction * vel_comp.velocity.length()
 	vel_comp.accelerate_to(target, direction, delta)
-	vel_comp.velocity = vel_comp.velocity.limit_length(vel_comp.velocity.length() - delta * 8)
+	vel_comp.velocity = vel_comp.velocity.limit_length(vel_comp.velocity.length() - delta * 9)
 
 
 func on_enter_state() -> void:
@@ -37,9 +42,12 @@ func on_enter_state() -> void:
 	player.is_crouching = true
 	set_crouch_collision_shape()
 
+
 func on_exit_state() -> void:
 	player.is_crouching = false
 	set_stand_collision_shape()
 
+
 func boost() -> void:
-	vel_comp.velocity *= 1.5
+	var slide_vel : Vector3 = vel_comp.velocity.normalized() * base_speed * slide_boost_speed_multiplier
+	vel_comp.velocity = slide_vel
