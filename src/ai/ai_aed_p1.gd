cec7457110df6bda1ae00130ec02a1677dc642cc
tokss <toxiwe@mail.ru>
2026-06-08 09:30:00 +0300

    [AED-P1] Move from export to unique names in debug label

--- src/ai/ai_aed_p1.gd
+++ src/ai/ai_aed_p1.gd
@@ -1,6 +1,9 @@
 extends CharacterBody3D
 class_name AED_P1
 
+# Сделать абстракцию условно EnemyNPC, чтоб в нем были все этим атрибуты.
+# И заменить <>_trigger на flag1, flag2, flag3, flag4 и т.д 
+
 @export var follow_trigger : int = 30
 @export var melee_trigger : float = 3.0
 @export var idle_trigger : float = 30.0
