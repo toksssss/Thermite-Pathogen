0c122a840493358e09c3ec7bc8fc9ce3d9dfcfaf
tokss <toxiwe@mail.ru>
2026-06-26 14:34:30 +0300

    Add quit command

--- src/dev/console/commands_register.gd
+++ src/dev/console/commands_register.gd
@@ -2,3 +2,4 @@ class_name ConsoleCommandsRegister
 
 static func register_commands() -> void:
 	DevConsoleManager.instance.add_command("help", HelpCommand.new())
+	DevConsoleManager.instance.add_command("quit", QuitCommand.new())
---
+++ src/templates/Object/console_command.gd
@@ -0,0 +1,9 @@
+extends ConsoleCommand
+
+func _init() -> void:
+	name = ""
+	help_text = ""
+	help_text_short = ""
+
+func run_command(args: Array[String]) -> String:
+	return ""
