2b74a760abc6e5445050a5ff04afe9da4b3571c0
tokss <toxiwe@mail.ru>
2026-06-27 13:51:45 +0300

    Timer resets time after level change

---
+++ src/level_timer.gd
@@ -0,0 +1,30 @@
+extends Node
+class_name LevelTimer
+
+static var instance : LevelTimer:
+	get:
+		return GameplayManager.instance.level_timer
+
+var start_level_msec : int
+var current_level_msec : int:
+	get:
+		return Time.get_ticks_msec() - start_level_msec
+	set(v):
+		pass
+
+var current_level_sec : int:
+	get:
+		@warning_ignore("integer_division")
+		return (current_level_msec / 1000) % 60
+	set(v):
+		pass
+
+var current_level_min : int:
+	get:
+		@warning_ignore("integer_division")
+		return (current_level_msec / 60000) % 60
+	set(v):
+		pass
+
+func _ready() -> void:
+	LevelContainer.instance.level_changed.connect(func() -> void: start_level_msec = Time.get_ticks_msec())
