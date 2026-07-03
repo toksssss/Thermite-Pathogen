408079eb6a341a214e84861edbfc3e4983d2b366
tokss <toxiwe@mail.ru>
2026-07-03 22:35:56 +0300

    Fix grenade

--- src/grenade.gd
+++ src/grenade.gd
@@ -4,7 +4,7 @@ class_name Grenade
 @export var particles : GPUParticles3D
 @export var mesh_node : Node3D
 @export var debug_mesh : MeshInstance3D
-@export var hurtbox : Hurtbox
+@export var hurtbox : ManualHurtbox
 
 @export var damage : float = 30.0
 
@@ -14,10 +14,10 @@ func _ready() -> void:
 	
 	create_attack_data()
 	
-	hurtbox.monitoring = false
+	hurtbox.damage()
 
 func explode() -> void:
-	hurtbox.monitoring = true
+	hurtbox.damage()
 	
 	particles.restart()
 	debug_mesh.visible = true
--- src/player_abilities/behaviors/throw_ability.gd
+++ src/player_abilities/behaviors/throw_ability.gd
@@ -5,13 +5,10 @@ class_name ThrowAbilityBehavior
 @export var throw_power : float = 1.0
 
 func apply_behavior(player: Player) -> void:
-	var direction : Vector3 = (player.head.marker.global_basis * Vector3(0, 0, 1)).normalized()
+	var direction : Vector3 = (player.head.marker.global_basis * Vector3(0, 0, -1)).normalized()
 	var force : Vector3 = direction * throw_power + player.velocity / 5.0 
 	
 	var item : Throwable = item_scene.instantiate()
 	item.force = force
 	item.position = player.head.marker.global_position
-	player.get_tree().root.add_child(item)
-	
-	
-	
+	Utils.add_child_safe(item, LevelContainer.instance.current_level)
